e6ab4d8997a89a18ea358101bdb5405d121fead1
Simon Finney <simonjfinney@gmail.com>
2020-08-16 17:46:51 +0100

    chore(classnames): remove package dependency

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@
  * @copyright Simon Finney 2020
  */
 
-import classnames from "classnames";
 import React, { StrictMode, useEffect, useState } from "react";
 import { render } from "react-dom";
 import Helmet from "react-helmet";
-import Sound from "react-sound";
 
 import { description as title } from "../package.json";
 
-import sound from "./white-noise.mp3";
+import audio from "./audio.mp3";
 import "./index.scss";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState();
-  useEffect(() => setIsLoading(true), []);
+  const [isLoaded, setIsLoaded] = useState();
+  useEffect(() => setIsLoaded(true), []);
 
   return (
     <StrictMode>
@@ -24,10 +22,14 @@ const App = () => {
         <meta name="description" content={title} />
         <title>{title}</title>
       </Helmet>
-      <main>
-        <h1 className={classnames({ loading: !isLoading })}>{title}</h1>
+
+      <main className={isLoaded ? null : "loading"}>
+        <h1>{title}</h1>
+
+        <audio autoPlay controls>
+          <source src={audio} />
+        </audio>
       </main>
-      <Sound loop={true} playStatus={Sound.status.PLAYING} url={sound} />
     </StrictMode>
   );
 };
